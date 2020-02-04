b73d56cafc4ad818d646d6f9594a3fdcacf0b0be
shengda567 <57602285+shengda567@users.noreply.github.com>
2020-02-04 13:09:56 -0500

    Update TestTriangle.py
    ID        Input      expected        actual             pass
                              result             result              or fail
    1.         3,4,5       right		InvalidInput	fail
    2.	   5,3,4       right		InvalidInput	fail
    3. 	   1,1,1	 Equilateral	InvalidInput  fail
    4. 	   6, 6, 7    Isosceles         InvalidInput  fail
    5.         5, 9, 9	Isosceles         InvalidInput  fail
    6.         5, 6, 7     Scalene         InvalidInput  fail
    7.         3, 7, 100   NotATriangle     InvalidInput  fail
    8.         5, 0, 9     InvalidInput      InvalidInput  pass
    9.         'I', 203, 162  InvalidInput    TypeError:    fail
    type error : '>' not supported between instances of 'str' and 'int'
    10.       5.5, 3, 3    InvalidInput      InvalidInput  pass

--- TestTriangle.py
+++ TestTriangle.py
@@ -9,7 +9,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 import unittest
 
-from Triangle import classifyTriangle
+from .Triangle import classifyTriangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
@@ -18,15 +18,35 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3, 4, 5),'Right','3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(5, 3, 4),'Right','5,3,4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1, 1, 1),'Equilateral','1,1,1 should be equilateral')
+
+    def testIsoscelesTrianglesA(self):
+        self.assertEqual(classifyTriangle(6, 6, 7), 'Isosceles')
+
+    def testIsoscelesTrianglesB(self):
+        self.assertEqual(classifyTriangle(5, 9, 9), 'Isosceles')
+
+    def testScaleneTriangles(self):
+        self.assertEqual(classifyTriangle(5, 6, 7), 'Scalene')
+
+    def testNotATriangle(self):
+        self.assertEqual(classifyTriangle(3, 7, 100), 'NotATriangle')
+
+    def testInvalidA(self):
+        self.assertEqual(classifyTriangle(5, 0, 9), "InvalidInput")
+
+    def testInvalidB(self):
+        self.assertEqual(classifyTriangle('I', 203, 162), "InvalidInput")
+
+    def testInvalidC(self):
+        self.assertEqual(classifyTriangle(5.5, 3, 3), "InvalidInput")
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
